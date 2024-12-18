17ac3a5d1e5ede543e75c9c415408207f62fcc7b
Z1Park <dongchen119@naver.com>
2024-12-18 22:56:46 +0900

    test: 포인트 조회 및 포인트 내역 조회 테스트 mock 사용하도록 수정

--- src/test/kotlin/io/hhplus/tdd/point/PointServiceUnitTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/PointServiceUnitTest.kt
@@ -2,11 +2,27 @@ package io.hhplus.tdd.point
 
 import io.hhplus.tdd.database.PointHistoryTable
 import io.hhplus.tdd.database.UserPointTable
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.*
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import org.mockito.junit.jupiter.MockitoExtension
 
+@ExtendWith(MockitoExtension::class)
 class PointServiceUnitTest {
+
+    @Mock
+    private lateinit var userPointTable: UserPointTable
+
+    @Mock
+    private lateinit var pointHistoryTable: PointHistoryTable
+
+    @InjectMocks
+    private lateinit var pointService: PointService
 
     /**
      * 포인트 조회에 대한 단위 테스트
@@ -14,46 +30,60 @@ class PointServiceUnitTest {
     @Nested
     inner class `포인트 조회` {
         /**
-         * 포인트 조회의 경우 UserPointTable의 메서드를 호출하여 반환하는 로직으로,
-         * 로직이 단순하여 Stub을 통한 상태 검증보다 Mock을 통한 행위 검증으로 테스트
+         * 포인트 조회 정상 동작 테스트
+         * mock을 통한 포인트 조회 로직의 상태 및 행위 검증
          */
         @Test
-        fun `UserPointTable의 조회 메서드를 호출한다`() {
+        fun `id를 통해 포인트를 조회한다`() {
             // given
-            val userPointTable = mock(UserPointTable::class.java)
-            val pointHistoryTable = mock(PointHistoryTable::class.java)
-            val sut = PointService(userPointTable, pointHistoryTable)
+            val userId = 1L
+            val userPoint = UserPoint(userId, 1000L, 30L)
+            `when`(userPointTable.selectById(userId)).thenReturn(userPoint)
 
             // when
-            sut.getUserPoint(1L)
+            val actual = pointService.getUserPoint(userId)
 
             // then
-            verify(userPointTable, times(1)).selectById(1L)
-            verifyNoMoreInteractions(userPointTable)
-            verifyNoInteractions(pointHistoryTable)
+            verify(userPointTable).selectById(1L)
+
+            assertThat(actual.id).isEqualTo(1L)
+            assertThat(actual.point).isEqualTo(1000L)
+            assertThat(actual.updateMillis).isEqualTo(30L)
         }
     }
 
     @Nested
     inner class `포인트 내역 조회` {
         /**
-         * 포인트 내역 조회의 경우 PointHistoryTable의 메서드를 호출하여 반환하는 로직으로,
-         * 로직이 단순하여 Stub을 통한 상태 검증보다 Mock을 통한 행위 검증으로 테스트
+         * 포인트 내역 조회 정상 동작 테스트
+         * mock을 통한 포인트 조회 로직의 상태 및 행위 검증
          */
         @Test
         fun `PointHistoryTable의 조회 메서드를 호출한다`() {
             // given
-            val userPointTable = mock(UserPointTable::class.java)
-            val pointHistoryTable = mock(PointHistoryTable::class.java)
-            val sut = PointService(userPointTable, pointHistoryTable)
+            val userId = 11L
+            val pointHistory1 = PointHistory(1L, userId, TransactionType.CHARGE, 1000L, 23L)
+            val pointHistory2 = PointHistory(2L, userId, TransactionType.USE, 800L, 23L)
+            `when`(pointService.getUserPointHistory(userId))
+                .thenReturn(listOf(pointHistory1, pointHistory2))
 
             // when
-            sut.getUserPointHistory(11L)
+            val userPointHistory = pointService.getUserPointHistory(userId)
 
             // then
-            verify(pointHistoryTable, times(1)).selectAllByUserId(11L)
-            verifyNoMoreInteractions(pointHistoryTable)
-            verifyNoInteractions(userPointTable)
+            verify(pointHistoryTable).selectAllByUserId(11L)
+
+            assertThat(userPointHistory).hasSize(2)
+
+            assertThat(userPointHistory[0].id).isEqualTo(1L)
+            assertThat(userPointHistory[0].userId).isEqualTo(11L)
+            assertThat(userPointHistory[0].type).isEqualTo(TransactionType.CHARGE)
+            assertThat(userPointHistory[0].amount).isEqualTo(1000L)
+
+            assertThat(userPointHistory[1].id).isEqualTo(2L)
+            assertThat(userPointHistory[1].userId).isEqualTo(11L)
+            assertThat(userPointHistory[1].type).isEqualTo(TransactionType.USE)
+            assertThat(userPointHistory[1].amount).isEqualTo(800L)
         }
     }
 }
